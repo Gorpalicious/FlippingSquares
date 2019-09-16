a0233cc0047517a2e056967cd2eb3d025d3a86dd
Gorpalicious <33858764+Gorpalicious@users.noreply.github.com>
2019-09-16 20:26:08 +0100

    more fixes to get it back into a reasonable state

--- dragCircle2.js
+++ dragCircle2.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-tabs */
+/* eslint-disable indent */
 const dragItem = document.querySelector('#dragCircle');
 const container = document.querySelector('#circleContainer');
 
@@ -10,12 +12,24 @@ let initialY;
 // var itemHeight;
 const upperPosBound = 388;
 const lowerPosBound = 0;
-var xOffset = upperPosBound / 2;
-var yOffset = upperPosBound / 2;
+let xOffset = upperPosBound / 2;
+let yOffset = upperPosBound / 2;
 
-container.addEventListener('mousedown', dragStart, false);
-container.addEventListener('mouseup', dragEnd, false);
-container.addEventListener('mousemove', drag, false);
+// note: can refactor this later!!!
+function getClosestNumber(numToCompare, lowNumber, highNumber) {
+	const a = highNumber - numToCompare;
+	const b = numToCompare - lowNumber;
+	// console.log(Math.max(highNumber, lowNumber));
+	if (Math.max(a, b) === a) {
+		return lowNumber;
+	}
+	return highNumber;
+}
+
+function setTranslate(xPos, yPos, el) {
+	const objectToTranslate = el;
+	objectToTranslate.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`;
+}
 
 function dragStart(e) {
 	initialX = e.clientX - xOffset;
@@ -26,7 +40,7 @@ function dragStart(e) {
 	}
 }
 
-function dragEnd(e) {
+function dragEnd() {
 	initialX = currentX;
 	initialY = currentY;
 
@@ -68,43 +82,54 @@ function drag(e) {
 	}
 }
 
-// note: can refactor this later!!!
-function getClosestNumber(numToCompare, lowNumber, highNumber) {
-	let a = highNumber - numToCompare;
-	let b = numToCompare - lowNumber;
-	console.log(Math.max(highNumber, lowNumber));
-	if (Math.max(a, b) === a) {
-		return lowNumber;
-	} else {
-		return highNumber;
-	}
+container.addEventListener('mousedown', dragStart, false);
+container.addEventListener('mouseup', dragEnd, false);
+container.addEventListener('mousemove', drag, false);
+
+// Adding Squares to page
+
+function addElement(elementType, classType, elementToFollow) {
+	// create a new div element
+	const newElement = document.createElement(elementType);
+	newElement.classList.add(classType);
+
+	const oldElementArray = document.querySelectorAll(`.${elementToFollow}`);
+	const oldElementLast = oldElementArray[oldElementArray.length - 1];
+	// var parentDiv = oldElementLast.parentNode;
+
+	// console.log(currentDiv);
+	// console.log(oldElementArray);
+	// console.log(oldElementLast);
+	// console.log(newElement);
+	document.body.append(newElement, oldElementLast.nextSibling);
 }
 
-function setTranslate(xPos, yPos, el) {
-	el.style.transform = 'translate3d(' + xPos + 'px, ' + yPos + 'px, 0)';
+for (let i = 0; i < 1; i += 1) {
+	addElement('div', 'squareContainer', 'squareContainer');
+	// console.log(i);
 }
 
-const squares = Array.from(document.querySelectorAll('.squareFront'));
+// const squares = Array.from(document.querySelectorAll('.squareFront'));
 
-// let squaresArray = Array.from(squares);
+// // let squaresArray = Array.from(squares);
 
-squares.forEach(squares => {
-	squares.addEventListener('mouseall', FollowMouse);
-});
+// squares.forEach((squares) => {
+//   squares.addEventListener('mouseall', FollowMouse);
+// });
 
-[...squares].forEach(squares => {
-	squares.addEventListener('mouseclick', SetShadows);
-});
+// [...squares].forEach((squares) => {
+//   squares.addEventListener('mouseclick', SetShadows);
+// });
 
-function FollowMouse() {
-	//get current squares origin
-	//get current mouse position
-	// set skew
-	// set shadow
-}
+// function FollowMouse() {
+//   // get current squares origin
+//   // get current mouse position
+//   // set skew
+//   // set shadow
+// }
 
-function SetShadows() {
-	//get current squares origin
-	//get current sun position
-	// set shadow
-}
+// function SetShadows() {
+//   // get current squares origin
+//   // get current sun position
+//   // set shadow
+// }
